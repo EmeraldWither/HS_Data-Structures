feeb419b882f8b786df80022d99814ddcad787a0
EmeraldWither <68785503+EmeraldWither@users.noreply.github.com>
2023-09-26 12:15:52 +0000

    bubble sort classwork

--- Engineers Notebook/src/HelloWorld.java
+++ Engineers Notebook/src/HelloWorld.java
@@ -1,5 +1,19 @@
 public class HelloWorld {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+
+        int [] values = new int[20];
+        for (int i = 0; i < values.length; i++) {
+            values[i] = i;
+        }
+        int pos = 5;
+        int iterations = 0;
+        for(int i = pos + 1; i < values.length; i++) {
+            values[i - 1] = values[i];
+            iterations+=2;
+        }
+        System.out.println(iterations);
+
+        
     }
 }
