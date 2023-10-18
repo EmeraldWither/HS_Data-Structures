c37d8178ef007fbc9ac312ca3b23bf606b34f9c1
EmeraldWither <68785503+EmeraldWither@users.noreply.github.com>
2023-10-18 19:04:12 +0000

    do adding test cases

--- Polynomial/src/PolynomialProgram.java
+++ Polynomial/src/PolynomialProgram.java
@@ -1,12 +1,15 @@
 public class PolynomialProgram {
     public static void main(String[] args) throws Exception {
-        Polynomial p = new Polynomial(new Term(4, 1)); //4x
-        p.add(new Polynomial(new Term(7, 1))); //7x
-        p.add(new Polynomial(new Term(18, 1))); //18x
-        p.add(new Polynomial(new Term(2, 7))); //2x^9272
-        p.add(new Polynomial(new Term(18, 0))); //18
-        p.add(new Polynomial(new Term(20, 0))); //20
+        Polynomial p = new Polynomial(new Term(3, 2)); //4x
+        p.add(new Polynomial(new Term(2, 1))); //7x
 
-        System.out.println(p);
+        Polynomial p2 = new Polynomial(new Term(3, 1));
+        p2.add(new Polynomial(new Term(2,2)));
+                p2.add(new Polynomial(new Term(3,0)));
+        p2.print();
+
+        p.add(p2);
+
+        p.print();
     }
 }
