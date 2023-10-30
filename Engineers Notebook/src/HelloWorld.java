0f2c280322477a40cc9c66ef93dd8c189d4fa0ba
EmeraldWither <68785503+EmeraldWither@users.noreply.github.com>
2023-10-30 22:58:52 +0000

    prime numbers

--- Engineers Notebook/src/HelloWorld.java
+++ Engineers Notebook/src/HelloWorld.java
@@ -1,23 +1,14 @@
-import java.util.HashSet;
-import java.util.Set;
-import java.util.TreeSet;
+import java.util.LinkedList;
 
 public class HelloWorld {
     public static void main(String[] args) throws Exception {
-        Set<String> friends = new HashSet<>();
-        friends.add("Gavin");
-        friends.add("Bhavi");
-        friends.add("Emilie");
-        System.out.println(friends);
+        LinkedList<String> staff = new LinkedList<String>();
+        staff.addFirst("Harry");
+        staff.addLast("Diana");
+        staff.addFirst("Tom");
+        System.out.println(staff.removeLast());
+        System.out.println(staff.removeFirst());
+        System.out.println(staff.removeLast());
 
-        friends.add("Ugh");
-        friends.add("VII");
-        for(String s : friends){
-            System.out.println(s.hashCode());
-        }
-        Set<String> treeSet = new TreeSet<>();
-        treeSet.addAll(friends);
-
-        System.out.println(treeSet);
     }
 }
