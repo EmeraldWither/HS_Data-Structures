7dd532b5fe6bd08411cbf7a47f340151efcb4871
EmeraldWither <68785503+EmeraldWither@users.noreply.github.com>
2023-11-30 14:50:36 +0000

    oops we need a scanner

--- Calculator/src/CalculatorProgram.java
+++ Calculator/src/CalculatorProgram.java
@@ -3,12 +3,17 @@
  * Data Structures Period 2
  * 11/30/23
  */
+
+import java.util.Scanner;
+
 public class CalculatorProgram {
     public static void main(String[] args) {
-        System.out.println("Value of 3 + 4: " + new Calculator("3 + 4").eval());
-        System.out.println("Value of 3 * 4 + 2: " + new Calculator("3 * 4 + 2").eval());
-        System.out.println("Value of (3 + 5) * 2: " + new Calculator("(3 + 5) * 2").eval());
-        System.out.println("Value of (7 * 2) / 5 + 1: " + new Calculator("(7 * 2) / 5 + 1").eval());
-        System.out.println("Value of (7 * -2) / 5 + 1: " + new Calculator("(7 * -2) / 5 + 1").eval());
+        Scanner in = new Scanner(System.in);
+        while(true){
+            System.out.print("Enter in an expression (\"exit\" to exit): ");
+            String input = in.nextLine();
+            if(input.equals("exit")) break;
+            System.out.println("The value is: " + new Calculator(input).eval());
+        }
     }
 }
