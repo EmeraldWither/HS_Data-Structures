6032a40887010b01e2d9b3b4186945628df2247c
EmeraldWither <68785503+EmeraldWither@users.noreply.github.com>
2023-12-12 16:36:18 +0000

    add test cases to linkedlists

--- LinkedLists/src/LinkedListsMain.java
+++ LinkedLists/src/LinkedListsMain.java
@@ -1,12 +1,26 @@
 public class LinkedListsMain {
     public static void main(String[] args) throws Exception {
+        testCase1();
+        System.out.println();
+        testCase2();
+    }
+    public static void testCase1() {
         LinkedList<String> list = new LinkedList<>();
         list.addFirst("E");
         list.addFirst("D");
         list.addFirst("C");
         list.addFirst("B");
         list.addFirst("A");
-        System.out.println(list);
+        list.print();
         list.reverse();
+        list.print();
+    }
+    public static void testCase2() {
+        LinkedList<String> list = new LinkedList<>();
+        list.addFirst("B");
+        list.addFirst("A");
+        list.print();
+        list.reverse();
+        list.print();
     }
 }
